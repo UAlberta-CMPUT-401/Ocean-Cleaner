fe6801b94aa6d43c4a2b78f702f167518bdb32b1
Maristella <hyunseo@ualberta.ca>
2021-01-23 11:27:59 -0700

    settings for template dir

--- ocean_cleaner/settings.py
+++ ocean_cleaner/settings.py
@@ -66,6 +66,10 @@ TEMPLATES = [
         },
     },
 ]
+SETTINGS_PATH = os.path.normpath(os.path.dirname(__file__))
+TEMPLATE_DIRS = (
+    os.path.join(SETTINGS_PATH, 'templates'),
+)
 
 WSGI_APPLICATION = 'ocean_cleaner.wsgi.application'
 
