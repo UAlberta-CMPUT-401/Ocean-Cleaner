b37f52a32b309d7a10dc918f1a9468566d7d1f11
Khang <khang.nguyen5700@gmail.com>
2021-01-23 16:23:36 -0700

    did stuff

--- main/views.py
+++ main/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
 
 def home(request):
-    return render(request, 'home.html', {})
+    context = {'title': 'Home'} # add more stuff here
+
+    return render(request, 'home.html', context)
+
+
+def about(request):
+    context = {'title': 'About'}
+    # Other programs can be called here and we can output the results on the page by providing it in the dictionary
+    return render(request, 'about.html', context)
